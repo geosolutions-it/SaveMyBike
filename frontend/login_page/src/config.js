0736e2be174cfaa50a609347365a50b9699dbe77
giohappy <giohappy@gmail.com>
2018-09-04 11:28:21 +0200

    cleanup

--- frontend/login_page/src/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  region: 'us-west-2',
-  IdentityPoolId: 'us-west-2:28fe9aee-83c7-42a9-8129-325ccd5fd10c',
-  UserPoolId: 'us-west-2_E1i57PuDf',
-  ClientId: '69pkdd67bnvko38n9fgftrdt15',
-}
